fcdea7bb433b03593f2bcdbcd394c444e78d1f94
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-01-04 22:43:02 -0600

    added three functions for employees, prompt function.

--- Develop/app.js
+++ Develop/app.js
@@ -10,10 +10,136 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function Intern() {
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                name: 'Name',
+                message: "What is the Intern's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the Intern's employee ID?",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the Intern's email address?",
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "What school does the Intern go to?",
+            }
+        ]).then(input => {
+            const intern = new Intern(input.Name, input.id, input.email, input.school);
+            employees.push(intern);
+            addEmployee();
+        },
+    )
+}
 
+function Manager() {
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                name: 'Name',
+                message: "What is the Manager's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the Manager's employee ID?",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the Manager's email address?",
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: "What is the Manager's Office number?",
+            }
+        ]).then(input => {
+            const manager = new Manager(input.Name, input.id, input.email, input.officeNumber);
+            employees.push(manager);
+            addEmployee();
+        },
+    )
+}
+
+function Engineer() {
+    inquirer.prompt(
+        [
+            {
+                type: 'input',
+                name: 'Name',
+                message: "What is the Engineer's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the Engineer's employee ID?",
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the Engineer's email address?",
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is the Engineer's Github account?",
+            }
+        ]).then(input => {
+            const engineer = new Engineer(input.Name, input.id, input.email, input.github);
+            employees.push(engineer);
+            addEmployee();
+        },
+    )
+}
+
+function addEmployee() {
+    inquirer.prompt(
+        [
+            {
+                type: "list",
+                name: "type",
+                choices: ["Engineer", "Intern", "Manager", "None at the moment"],
+                message: "Choose what style of employee you would like to add to your list. If none, choose 'none at the moment' option.",
+            }
+    ]).then(input => {
+        if (input.choices === "Engineer") {
+            Engineer();
+        }
+        else if (input.choices === "Intern") {
+            Intern();
+        }
+        else if (input.choices === "Manager") {
+            Manager();
+        }
+        else if (input.choices === "None") {
+            createEmployee();
+        }
+    })
+};
+
+function createEmployee() {
+    inquirer.prompt()
+    .then((employee) => writeToFile('team.html', generate));
+}
+
+function writeToFile(outputPath, employee) {
+    return fs.writeFileSync(outputPath, render(employee));
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
